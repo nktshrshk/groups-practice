8840a13a005dfefab4951639eced30b6fde2655f
nktshrshk <163222741+nktshrshk@users.noreply.github.com>
2024-03-20 12:07:26 +0300

    Merge classroom_feature with main
    Add: Classroom class and PrintInfo method for it
    add: PrintInfoForClassroom method for Student and Teacher classes
    add: Classroom initialization in Program
    qol: beautify console app output

---
+++ Implementations/Classroom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groups_practice
+{
+    class Classroom
+    {
+        public string Name;
+        public Student Student;
+        public Teacher Teacher;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"This is {Name} classroom. It contains:");
+            Teacher.PrintInfoForClassroom();
+            Student.PrintInfoForClassroom();
+        }
+    }
+}
